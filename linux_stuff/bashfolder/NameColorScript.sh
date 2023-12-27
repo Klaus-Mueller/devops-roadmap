0560c7db31cd46da0b91fb7be23262aa6a7a7e9c
klaus <klausmueller@outlook.com>
2023-12-27 17:24:05 -0300

    compress files

---
+++ linux_stuff/bashfolder/NameColorScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hello, Whats your name?"
+
+read name
+
+echo "What is your favorite color?"
+
+read color
+
+echo "Welcome" $name "I can see your favorit color is" $color "I whish you a happy day!"
